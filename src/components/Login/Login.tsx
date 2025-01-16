c067fbd3446b9c3fe8d1e5c3886188861abe41bc
Ellis Osen <Ellis@abitslanted.com>
2025-01-16 11:06:29 -0600

    Added Authentication layer using Google Auth and Next Auth  Changes to be committed: 	modified:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	new file:   src/components/Header/Header.tsx 	renamed:    src/pages/components/Header/index.ts -> src/components/Header/index.ts 	new file:   src/components/Login/Login.tsx 	renamed:    src/pages/login/index.ts -> src/components/Login/index.ts 	renamed:    src/pages/components/SideMenu/SideMenu.tsx -> src/components/SideMenu/SideMenu.tsx 	renamed:    src/pages/components/SideMenu/index.ts -> src/components/SideMenu/index.ts 	modified:   src/pages/_app.tsx 	new file:   src/pages/api/auth/[...nextauth].ts 	deleted:    src/pages/components/Header/Header.tsx 	modified:   src/pages/index.tsx 	deleted:    src/pages/login/Login.tsx

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@mui/material";
+import { useSession, signIn, signOut } from "next-auth/react"
+
+const Login = ( ) => {
+    const { data: session } = useSession();
+    
+
+    if (session) {
+        return <>
+            Signed in as {session?.user?.email}
+            <Button variant={'contained'} color={'error'} onClick={() => signOut()}>Sign Out</Button>
+        </>
+        
+      }
+    return <>
+          <h2>Please Log In</h2> <br />
+          <Button variant={'contained'} color={'success'} onClick={() => signIn()}>Sign in</Button>
+    </>
+      
+}
+
+export default Login;
+
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,80 +1,41 @@
-import Image from "next/image";
+import Login from "@/components/Login";
 import Dashboard from "./dashboard";
-import { Geist, Geist_Mono } from "next/font/google";
-import Login from "./login";
-import Header from "./components/Header";
-import SideMenu from "./components/SideMenu";
+import Header from "@/components/Header";
+import SideMenu from "@/components/SideMenu";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Header />
-        <SideMenu />
-        <Dashboard/>
+  const { data: session } = useSession();
 
+  return (
+    <>
+      <Head>
+        <title>Data Dashboard</title>
+        <meta name="description" content="Data Dashboard"/>
+        <meta name ="viewport" content="width=devide-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+        <Header />
+        {
+          session && (
+            <>
+              <SideMenu />
+              <Dashboard/>
+            </>
+            
+
+          )
+        }
+        <Login />
+        
 
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    
+    </>
+    
   );
 }
