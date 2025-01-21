7ca84815836a4e9ddf33f6fe4c69f21e6e1ee82e
Ellis Osen <Ellis@abitslanted.com>
2025-01-20 23:55:05 -0600

    added gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/cd-dashboard-react/" : "",
+  basePath: isProd ? "/cd-dashboard-react" : "",
+  output: "export",
 };
 
 export default nextConfig;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,24 +1,24 @@
-import NextAuth, {NextAuthOptions} from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   // taken from https://youtu.be/g95MmAczBUg?si=f4HQHZf933Xln_EZ&t=198
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       authorization: {
         params: {
-            prompt: 'consent',
-            access_type: 'offline',
-            response_type: 'code',
-            token_endpoint_auth_method: "none"
-        }
-      }
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+          token_endpoint_auth_method: "none",
+        },
+      },
     }),
     // ...add more providers here
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
